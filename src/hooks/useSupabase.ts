2876bd172ae34b4c2811495e543381039b4968d0
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2024-02-01 20:18:37 +0300

    Hooks

---
+++ src/hooks/useSupabase.ts
@@ -0,0 +1,18 @@
+import { supabase } from "@/lib/supabase";
+import { useState } from "react";
+
+export const useViews=()=>{
+    const [views, setViews]= useState<any[]>([])
+
+    const getViews= async()=>{
+        const {data, error}= await supabase.from('views')
+        .select('*')
+
+        if(data){
+            setViews(data)
+        }
+    }
+    return {
+        views, setViews
+    }
+}
